f8f0989cd0e874ad30fc925def00fd108a8fac1d
LUNAZAMORA <jclunazamora@gmail.com>
2023-01-18 02:41:26 -0600

    Some tests

--- pruebas.cpp
+++ pruebas.cpp
@@ -1,30 +1,41 @@
 #include <iostream>
 #include <bitset>
+#include "types.h"
+
+#define get_bit_at(num, i) (1ULL << i) & (num)? 1 : 0
+#define set_bit(num, i) ((num) |= (1ULL << i))
+
 typedef uint64_t U64;
 
-enum p1{
-    a,
-    b,
-    c,
-    d
-};
+//1 or x = 1 
+// 0 or x = x
+// 1 and x = x
+// 0 and x = = 0
+void print_board_repr(){
+    int matrix[8][8];
+    int count = 0;
+    for(int i = 0; i<8; i++){
+        for(int j = 0; j< 8;j++){
+            matrix[i][j] = count++;
+        }
+    }
+    for(int i = 0; i<8; i++){
+        for(int j = 0; j< 8;j++){
+            std::cout << matrix[i][j] << "  ";
+        }
+        std::cout << std::endl;
+    }
+}
 
 int main(){
-    /*
-    std::cout << sizeof(long long) << std::endl;
-    std::bitset<10> t1 {"110"};
-    U64 t2 = 110;
-    std::cout << "Before shift " << t1.to_string() << std::endl;
-    t1 >>= 1;
-    std::cout << "After shift " << t1.to_string() << std::endl;
-
-    std::cout << "Using the uint64_t " << std::endl;
-    std::cout << "Before shift " << std::hex << t2 << std::endl;
-    t2 <<= 1;
-    std::cout << "After shift " << std::hex<<t2 << std::endl;
-    */
-    std::cout << a << std::endl;
-    std::cout << c << std::endl;
+    print_board_repr();
+    U64 WQueen = 0x0;
+    set_bit(WQueen, d4);
+    std::cout << std::hex << WQueen << std::endl;
+    std::cout << "Testing function :" << std::endl;
+    for(int i = 0; i < 64; i++){
+        printf("Bit at: %d is: %d\n", i, get_bit_at(WQueen, i));
+    }
     return 0;
 }
 
--- types.h
+++ types.h
@@ -23,7 +23,38 @@ enum PieceType
    pt_king   = 7,
 };
 
+enum enumFile {
+  A_File = 0,
+  B_File = 1,
+  C_File = 2,
+  D_File = 3,
+  E_File = 4,
+  F_File = 5,
+  G_File = 6,
+  H_File = 7,
+};
 
+
+enum enumRank {
+  Rank_1 = 0,
+  Rank_2 = 1,
+  Rank_3 = 2,
+  Rank_4 = 3,
+  Rank_5 = 4,
+  Rank_6 = 5,
+  Rank_7 = 6,
+  Rank_8 = 7,
+};
+enum enumSquare {
+  a1, a2, a3, a4, a5, a6, a7, a8,
+  b1, b2, b3, b4, b5, b6, b7, b8,
+  c1, c2, c3, c4, c5, c6, c7, c8,
+  d1, d2, d3, d4, d5, d6, d7, d8,
+  e1, e2, e3, e4, e5, e6, e7, e8,
+  f1, f2, f3, f4, f5, f6, f7, f8,
+  g1, g2, g3, g4, g5, g6, g7, g8,
+  h1, h2, h3, h4, h5, h6, h7, h8
+};
 enum Moves{
     
 };
